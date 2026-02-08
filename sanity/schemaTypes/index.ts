a886a1fa8ad53c8daa9bc4cfea96e9b189537369
ZIN MYO SWE <overmidnight12@gmail.com>
2026-02-09 01:39:38 +0700

    Add CategoryType, courseType, LessonType, ModuleType for sanity and Sanity mux video

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { categoryType } from "./categoryType";
+import { courseType } from "./courseType";
+import { lessonType } from "./lessonType";
+import { moduleType } from "./moduleType";
+// import { noteType } from "./noteType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [courseType, moduleType, lessonType, categoryType],
 }
